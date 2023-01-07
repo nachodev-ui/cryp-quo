9209c26cf28657d4fc5f109dcf273e2481621b2e
cmnaxo <ig.cisternas@duocuc.cl>
2023-01-07 16:30:27 -0300

    CRYPTO-ENDED

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    { id: 'USD', nombre: 'Dólar Estadounidense' },
+    { id: 'CLP', nombre: 'Peso Chileno' },
+    { id: 'EUR', nombre: 'Euro' },
+    { id: 'MXN', nombre: 'Peso Mexicano' },
+];
+
+export {
+    monedas
+}
